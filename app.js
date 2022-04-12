8da11bf4283167877fc789e9aa5f10b4d910037b
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-12 18:23:57 +0200

    Added some javascript and the footer section

--- app.js
+++ app.js
@@ -53,3 +53,51 @@ const checkFAQClick = () => {
 }
 
 checkFAQClick()
+
+
+const checkEmailValidation = () => {
+    const form = document.getElementById("form")
+    const email = document.getElementById("email-input").value
+    const text = document.getElementById("text")
+    const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const contactBtn = document.querySelector(".contact-btn")
+
+
+
+
+contactBtn.addEventListener('click', () => {
+
+    if (email.match(pattern)){
+        form.classList.add("valid")
+        form.classList.remove("invalid")
+     
+     
+        text.innerHTML = "Your email adress is valid !"
+        text.style.color = "#00ff00"
+        
+     }
+
+
+
+    if (!email.match(pattern)) {
+        form.classList.add("invalid")
+        form.classList.remove("invalid")
+     
+        text.innerHTML = "Please enter a valid email adress !"
+        text.style.color = "#fff"
+     }
+
+
+    if (email == ""){
+        text.innerHTML = ""
+    }
+    
+
+    
+
+})
+ 
+
+}
+
+ checkEmailValidation()
